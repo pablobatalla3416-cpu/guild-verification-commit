db967ce7eece39eec195c45b06baef92c54fb6df
pablobatalla3416-cpu <pablobatalla3416@gmail.com>
2026-03-12 15:33:14 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T12:32:57.671Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T12:33:15.853Z
+Commit: Fix memory leak in event handlers
