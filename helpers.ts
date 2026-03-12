282be561801af645b7b6df5267516dae1359f911
pablobatalla3416-cpu <pablobatalla3416@gmail.com>
2026-03-12 15:40:04 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T12:32:49.381Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-12T12:40:05.677Z
+Commit: Patch API endpoint error handling
